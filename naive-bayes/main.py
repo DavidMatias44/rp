2c044533543ac7c0f7a203c2ce6b44bcdf0cb41e
DavidMatias44 <daosmaru3667@gmail.com>
2025-05-07 07:41:41 -0600

    mmmm... si

---
+++ naive-bayes/main.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+
+def naive_bayes(X, m_hat, s_hat):
+    pass
+
+
+def obten_parametros(X):
+    pass
+
+
+def main():
+    print("asdf")
+    pass
+
+
+if __name__ == "__main__":
+    main()
